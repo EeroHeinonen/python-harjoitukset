0878e02f631cac872797cfb21c0b5a02aad1a0cd
EeroHeinonen <eero.heinonen00@hotmail.com>
2026-08-25 13:34:28 +0300

    Added "addItem" and "showInventory" functions to player.py

--- Assignments/peliprojekti/player.py
+++ Assignments/peliprojekti/player.py
@@ -4,20 +4,22 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 command = ""
 difficulty = "Normal"
+inventory = []
+item = "0"
 
 def settings():
     global difficulty
     system("cls")
     print("Settings available: \nDifficulty")
-    print('"Back" to go back')
+    print('"Back" or nothing to go back')
     command = input("Enter a setting to change: ")
-
+    r
     if command == "Difficulty" or command == "difficulty":
 
         system("cls")
         print("Current difficulty: " + difficulty)
         print("Difficulty settings: \nEasy \nNormal \nHard")
-        print('"Back" to go back')
+        print('"Back" or nothing to go back')
         command = input("Enter a difficulty to change: ")
         if command == "Easy" or command == "easy":
             difficulty = "Easy"
@@ -25,12 +27,32 @@ def settings():
             difficulty = "Normal"
         elif command == "Hard" or command == "hard":
             difficulty = "Hard"
-        elif command != "Back" or command != "back":
+        elif command != "Back" or command != "back" or command != "BACK":
             pass
 
         
 
     return difficulty
+
+def addItem():
+    system("cls")
+    global item
+    while item != "":
+        item = input('Input item to be added (Or "back" / nothing to go back): ')
+        if item != "back" and item != "Back" and item != "BACK":
+            if item != "":
+                inventory.append(item)
+        else:
+            pass
+
+    return item
+
+def showInventory():
+    system("cls")
+    global item
+    for item in inventory:
+        print(f"- {item}")
+    input('Press any key to continue...')
 
 if age < 12:
     print("The minimum age is 12!")
@@ -46,14 +68,22 @@ else:
 
 
                 """)
-        print("Available commands: \nPlay (play) \nSettings (settings) \nQuit (quit, exit, stop)")
+        print("Available commands: \nPlay (play) \nInventory (add item, show inventory) \nSettings (settings) \nQuit (quit, exit, stop)")
         command = input("Enter a command: ")
         if command == "quit" or command == "exit" or command == "stop":
             break
-        elif command == "settings":
+        elif command == "settings" or command == "Settings" or command == "SETTINGS":
             settings()
-
+        elif command == "add item" or command == "Add Item" or command == "Add item" or command == "add Item" or command == "ADD ITEM" or command == "additem" or command == "Additem"or command == "addItem" or command == "ADDITEM":
+            addItem()
+        elif command == "inventory" or command == "Inventory" or command == "INVENTORY":
+            showInventory()
         elif command == "Play" or command == "play":
             system("cls")
             print("Not yet available!")
+            input("Press any key to continue...")
             ## Game code tba
+        else:
+            system("cls")
+            print("Invalid command!")
+            input("Press any key to continue...")
