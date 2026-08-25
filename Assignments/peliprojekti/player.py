888df9e509d561852c263f945e0c5e4dc258030a
EeroHeinonen <eero.heinonen00@hotmail.com>
2026-08-25 14:55:53 +0300

    Added precautions when asking name to player.py.

--- Assignments/peliprojekti/player.py
+++ Assignments/peliprojekti/player.py
@@ -1,7 +1,6 @@
 from os import system
 
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
 command = ""
 difficulty = "Normal"
 inventory = []
@@ -75,41 +74,56 @@ def showInventory():
         print(f"- {item}")
     input('Press any key to continue...')
 
-if age < 12:
+while name == "":
     system("cls")
-    print("The minimum age is 12!")
-else:
-    while command.strip().casefold() != "stop" or command.strip().casefold() != "exit" or command.strip().casefold() != "quit":
-        system("cls")
-        print(r"""
-
-                                         _  _  ____  __     ___  __   _  _  ____ 
-                                        / )( \(  __)(  )   / __)/  \ ( \/ )(  __)
-                                        \ /\ / ) _) / (_/\( (__(  O )/ \/ \ ) _) 
-                                        (_/\_)(____)\____/ \___)\__/ \_)(_/(____)
-
-
-                """)
-        print("Available commands: \n\n- Play (play) \n- Settings (settings) \n- Quit (quit, exit, stop)\n")
-        command = input("Enter a command: ")
-        if command.strip().casefold() == "":
+    print("You can not input an empty name!")
+    input("Press any key to continue...")
+    system("cls")
+    name = input("Enter your name: ")
+    if name != "":
+        age = input("Enter your age: ")
+        while age == "":
             system("cls")
-            print("Invalid command!")
+            print("You can not input an empty age!")
             input("Press any key to continue...")
-        elif command.strip().casefold() == "settings":
-            settingsFunc()
-        elif command.strip().casefold() == "play":
             system("cls")
-            print("Available commands \n\n- Add item \n- Inventory (Show inventory)\n")
-            command = input("Enter a command: ")
-            if command.strip().casefold() == "additem" or command.strip().casefold() == "add item":
-                addItem()
-            elif command.strip().casefold() == "inventory":
-                showInventory()
-            ## Game code tba
-            else:
-                system("cls")
-                print("Invalid command!")
-                input("Press any key to continue...")
+            age = input("Enter your age: ")
+        
+        if int(age) < 12:
+            system("cls")
+            print("The minimum age is 12!")
         else:
-            break
+            while command.strip().casefold() != "stop" or command.strip().casefold() != "exit" or command.strip().casefold() != "quit":
+                system("cls")
+                print(r"""
+
+                                                 _  _  ____  __     ___  __   _  _  ____ 
+                                                / )( \(  __)(  )   / __)/  \ ( \/ )(  __)
+                                                \ /\ / ) _) / (_/\( (__(  O )/ \/ \ ) _) 
+                                                (_/\_)(____)\____/ \___)\__/ \_)(_/(____)
+
+
+                        """)
+                print("Available commands: \n\n- Play (play) \n- Settings (settings) \n- Quit (quit, exit, stop)\n")
+                command = input("Enter a command: ")
+                if command.strip().casefold() == "":
+                    system("cls")
+                    print("Invalid command!")
+                    input("Press any key to continue...")
+                elif command.strip().casefold() == "settings":
+                    settingsFunc()
+                elif command.strip().casefold() == "play":
+                    system("cls")
+                    print("Available commands \n\n- Add item \n- Inventory (Show inventory)\n")
+                    command = input("Enter a command: ")
+                    if command.strip().casefold() == "additem" or command.strip().casefold() == "add item":
+                        addItem()
+                    elif command.strip().casefold() == "inventory":
+                        showInventory()
+                    ## Game code tba
+                    else:
+                        system("cls")
+                        print("Invalid command!")
+                        input("Press any key to continue...")
+                else:
+                    break
