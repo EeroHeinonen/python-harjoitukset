8a91c747de14c98afd8d8027156a505b4cda9595
EeroHeinonen <eero.heinonen00@hotmail.com>
2026-09-09 13:23:55 +0300

    Modified pizzaPricePerUnit.py in Mod 07 and modified elevator.py in Mod 10

--- Assignments/Mod 07/pizzaPricePerUnit.py
+++ Assignments/Mod 07/pizzaPricePerUnit.py
@@ -1,22 +1,81 @@
 import math
-userIn = 0
+from os import system
+userIn = ""
+price = ""
+amount = ""
+i = 0
+p = 0
+
+pizzaList = []
 
 def calculate(userIn, price):
     
-    pizzaArea = math.pi * (userIn / 2) ** 2
+    pizzaArea = math.pi * ((userIn / 100) / 2) ** 2
     pizzaCalc = price / pizzaArea
     return pizzaCalc
 
-userIn = input("Input the diameter of the first pizza: ")
-price = input("Input the price of the first pizza: ")
-pizzaOne = calculate(int(userIn), int(price))
+while amount == "":
+    system("cls")
+    try:
+        amount = int(input("Input the amount of pizzas to compare: "))
+        if amount <= 1:
+            system("cls")
+            print("The amount must be greater than 1!")
+            input("Press any key to continue...")
+            amount = ""
+    except :
+        system("cls")
+        print("The value is not a number!")
+        input("Press any key to continue...")
+        amount = ""
 
-userIn = input("Input the diameter of the second pizza: ")
-price = input("Input the price of the second pizza: ")
-pizzaTwo = calculate(int(userIn), int(price))
-
-if pizzaOne < pizzaTwo:
-    print(f"The first pizza is a better deal at {pizzaOne:2.3f} €/m²!")
-    
 else:
-    print(f"The second pizza is a better deal at {pizzaTwo:2.3f} €/m²!")
+    while i < amount:
+        try:
+            system("cls")
+            userIn = float(input(f"Input the diameter of pizza {i + 1} in cm: "))
+
+            try:
+                system("cls")
+                price = float(input(f"Input the price of pizza {i + 1} in €: "))
+
+                if userIn != "" and price != "":
+                    pizza = calculate(userIn, price)
+                    pizzaList.append(pizza)
+                    i += 1
+
+            except:
+                system("cls")
+                price = ""
+                print("Value is not a number!")
+                input("Press any key to continue...")
+                system("cls")
+
+        except:
+            system("cls")
+            userIn = ""
+            print("Value is not a number!")
+            input("Press any key to continue...")
+            system("cls")
+
+    else:
+        c = p + 1
+        cheapest = pizzaList[p]
+        try:
+            for pizza in pizzaList:
+                try:
+                    if pizzaList[p] <= cheapest:
+                        cheapest = pizzaList[p]
+                        c = p + 1
+                    else:
+                        cheapest = cheapest
+                except :
+                    pass
+            
+                print(pizza)
+                p += 1
+
+            print(f"Pizza no. {c} is the best deal at {cheapest:2.2f} €/m²!")
+
+        except :
+            pass
--- Assignments/Mod 10/elevator.py
+++ Assignments/Mod 10/elevator.py
@@ -1,3 +1,94 @@
+from os import system
+
+highestFloor = ""
+lowestFloor = ""
+targetFloor = ""
+
 class Elevator:
     def __init__(self, currentFloor, highestFloor, lowestFloor):
-        self.currentFloor 
+        self.currentFloor = lowestFloor
+        self.highestFloor = highestFloor
+        self.lowestFloor = lowestFloor
+
+    def moveUp(self):
+        self.currentFloor += 1
+
+    def moveDown(self):
+        self.currentFloor -= 1
+
+while highestFloor == "":
+    system("cls")
+    try:
+        highestFloor = int(input("Enter the highest floor for the elevator: "))
+        if highestFloor <= 0:
+            system("cls")
+            print(f"The value must be higher than 0!")
+            input("Press any key to continue...")
+            highestFloor = ""
+    except :
+        system("cls")
+        print("The value must be a number!")
+        input("Press any key to continue...")
+
+else:
+    while lowestFloor == "":
+        system("cls")
+        try:
+            lowestFloor = int(input("Enter the lowest floor for the elevator: "))
+            if lowestFloor >= highestFloor:
+                system("cls")
+                print(f'The value must be lower than the highest floor! ("{highestFloor}")')
+                input("Press any key to continue...")
+                lowestFloor = ""
+            elif lowestFloor >= 1:
+                system("cls")
+                print(f"The value must be lower than 1!")
+                input("Press any key to continue...")
+                lowestFloor = ""
+
+        except :
+            system("cls")
+            print("The value must be a number!")
+            input("Press any key to continue...")
+
+    else:
+
+        elev = Elevator(lowestFloor, highestFloor, lowestFloor)
+
+        while targetFloor != "q":
+            system("cls")
+            print('Enter "quit / q" to quit')
+            print(f"The current floor of the elevator is: {elev.currentFloor}")
+
+            targetFloor = input("Enter the target floor: ")
+
+            try:
+                if int(targetFloor) > elev.highestFloor:
+                    system("cls")
+                    print(f"Value is too high, the highest floor is {elev.highestFloor}!")
+                    input("Press any key to continue...")
+                elif int(targetFloor) < elev.lowestFloor:
+                    system("cls")
+                    print(f"Value is too low, the lowest floor is {elev.lowestFloor}!")
+                    input("Press any key to continue...")
+                elif int(targetFloor) == elev.currentFloor:
+                    system("cls")
+                    print(f"The elevator is already on floor {elev.currentFloor}!")
+                    input("Press any key to continue...")
+                elif elev.currentFloor > int(targetFloor):
+                    while elev.currentFloor > int(targetFloor):
+                        elev.moveDown()
+                    print(f"Elevator has reached the target floor {int(targetFloor)}!")
+                    input("Press any key to continue...")
+                elif elev.currentFloor < int(targetFloor):
+                    while elev.currentFloor < int(targetFloor):
+                        elev.moveUp()
+                    print(f"Elevator has reached the target floor {int(targetFloor)}!")
+                    input("Press any key to continue...")
+            except :
+                if targetFloor.strip().casefold() == "q" or targetFloor.strip().casefold() == "quit":
+                    pass
+                else:
+                    system("cls")
+                    print("The value is not a number!")
+                    input("Press any key to continue...")
