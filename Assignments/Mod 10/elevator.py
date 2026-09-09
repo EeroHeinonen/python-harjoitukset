48438da789a45c80e282fefb975531d8b5809f7d
EeroHeinonen <eero.heinonen00@hotmail.com>
2026-09-09 13:57:44 +0300

    Modified elevator.py

--- Assignments/Mod 10/elevator.py
+++ Assignments/Mod 10/elevator.py
@@ -1,4 +1,5 @@
 from os import system
+import time
 
 highestFloor = ""
 lowestFloor = ""
@@ -12,9 +13,13 @@ class Elevator:
 
     def moveUp(self):
         self.currentFloor += 1
+        print("Rising...\n")
+        time.sleep(0.75)
 
     def moveDown(self):
         self.currentFloor -= 1
+        print("Lowering...\n")
+        time.sleep(0.75)
 
 while highestFloor == "":
     system("cls")
@@ -37,7 +42,7 @@ else:
             lowestFloor = int(input("Enter the lowest floor for the elevator: "))
             if lowestFloor >= highestFloor:
                 system("cls")
-                print(f'The value must be lower than the highest floor! ("{highestFloor}")')
+                print(f'The value must be lower than the highest floor ("{highestFloor}")!')
                 input("Press any key to continue...")
                 lowestFloor = ""
             elif lowestFloor >= 1:
@@ -55,40 +60,72 @@ else:
 
         elev = Elevator(lowestFloor, highestFloor, lowestFloor)
 
-        while targetFloor != "q":
+        while targetFloor.strip().casefold() != "q":
             system("cls")
-            print('Enter "quit / q" to quit')
             print(f"The current floor of the elevator is: {elev.currentFloor}")
+            print('\nAvailable commands:\n- Up\n- Down\n- Target floor\n- "q" to quit\n')
 
-            targetFloor = input("Enter the target floor: ")
+            targetFloor = input("Enter command or target floor: ")
 
             try:
-                if int(targetFloor) > elev.highestFloor:
+                if targetFloor.strip().casefold() == "up":
+                    system("cls")
+                    if elev.currentFloor == highestFloor:
+                        print(f'The elevator can not move past the highest floor "({highestFloor})"!')
+                        input("Press any key to continue...")
+                    else:
+                        elev.moveUp()
+                        print(f"Elevator has reached floor {elev.currentFloor}!")
+                        input("Press any key to continue...")
+
+                elif targetFloor.strip().casefold() == "down":
+                    system("cls")
+                    if elev.currentFloor == lowestFloor:
+                        print(f'The elevator can not move past the lowest floor ("{lowestFloor}")!')
+                        input("Press any key to continue...")
+                    else:
+                        elev.moveDown()
+                        print(f"Elevator has reached floor {elev.currentFloor}!")
+                        input("Press any key to continue...")
+
+                elif int(targetFloor) > elev.highestFloor:
                     system("cls")
                     print(f"Value is too high, the highest floor is {elev.highestFloor}!")
                     input("Press any key to continue...")
+                    targetFloor = ""
+
                 elif int(targetFloor) < elev.lowestFloor:
                     system("cls")
                     print(f"Value is too low, the lowest floor is {elev.lowestFloor}!")
                     input("Press any key to continue...")
+                    targetFloor = ""
+
                 elif int(targetFloor) == elev.currentFloor:
                     system("cls")
                     print(f"The elevator is already on floor {elev.currentFloor}!")
                     input("Press any key to continue...")
+                    targetFloor = ""
+
                 elif elev.currentFloor > int(targetFloor):
+                    system("cls")
                     while elev.currentFloor > int(targetFloor):
                         elev.moveDown()
-                    print(f"Elevator has reached the target floor {int(targetFloor)}!")
+                    print(f"Elevator has reached floor {int(targetFloor)}!")
                     input("Press any key to continue...")
+
                 elif elev.currentFloor < int(targetFloor):
+                    system("cls")
                     while elev.currentFloor < int(targetFloor):
                         elev.moveUp()
-                    print(f"Elevator has reached the target floor {int(targetFloor)}!")
+                    print(f"Elevator has reached floor {int(targetFloor)}!")
                     input("Press any key to continue...")
+
             except :
-                if targetFloor.strip().casefold() == "q" or targetFloor.strip().casefold() == "quit":
-                    pass
+                if targetFloor.strip().casefold() == "q":
+                    system("cls")
+                    print("Goodbye!")
+
                 else:
                     system("cls")
-                    print("The value is not a number!")
+                    print("The command is not valid!")
                     input("Press any key to continue...")
