83b8d9b33baeb9da5e1a99958c62ebbd70b8c23e
EeroHeinonen <eero.heinonen00@hotmail.com>
2026-09-21 11:06:24 +0300

    Modified carRaceContinued.py

--- Assignments/Mod 10/carRaceContinued.py
+++ Assignments/Mod 10/carRaceContinued.py
@@ -5,6 +5,8 @@ import time
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 command = ""
 timeTraveled = 0
+winner = ""
+raceLen = ""
 
 cars = []
 
@@ -17,27 +19,26 @@ class Racing:
     def race(self):
         global timeTraveled
         for car in self.participants:
-
             changeInSpeed = random.randint(-10, 15)
             car.accelerate(changeInSpeed)
             car.travel(1)
-            R.raceOver()
 
         timeTraveled += 1
 
     def printCurrent(self):
-        if R.raceOver == False:
+        if R.raceOver() == False:
             for car in self.participants:
                 print(f"{car.registryNum} has so far traveled for {car.distanceTraveled} km(s) and current speed is {car.currentSpeed}\n")
 
             input("Press any key to continue...")
 
     def raceOver(self):
+        global winner
         for car in self.participants:
-            if car.distanceTraveled >= R.raceLen:
+            if car.distanceTraveled >= self.raceLen:
+                winner = car.registryNum
                 return True
-            else:
-                return False
+        return False
 
 
 class Car:
@@ -60,15 +61,18 @@ class Car:
 for i in range(10):
     cars.append(Car(f"{random.choice(letters) + random.choice(letters) + random.choice(letters)}-{str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))}", random.randint(100, 200), 0, 0))
 raceName = input("Please input the race name: ")
-raceLen = int(input("Please input the length of the race: "))
-system("cls")
-R = Racing(raceName, raceLen, cars)
-while command.strip().casefold() != "q":
-    print(R.raceOver())
-    if R.raceOver() == True:
-        print(f"The race is over! {car.registryNum} has won!")
-        break
-    else:
+while raceLen == "":
+    system("cls")
+    try:
+        raceLen = int(input("Please input the length of the race: "))
+    except ValueError:
+        system("cls")
+        print("The value must be a number!")
+        input("Press any key to continue...")
+else:
+    R = Racing(raceName, raceLen, cars)
+    while command.strip().casefold() != "q" and R.raceOver() == False:
+        system("cls")
         print(f"Race: {raceName}\nLength: {raceLen}\n\n")
         i = 1
         for car in R.participants:
@@ -79,6 +83,10 @@ while command.strip().casefold() != "q":
         if command.strip().casefold() == "race":
             while timeTraveled < 10:
                 R.race()
+                R.raceOver()
             else:
                 R.printCurrent()
                 timeTraveled = 0
+    else:
+        system("cls")
+        print(f"The race is over! {winner} has won!")
